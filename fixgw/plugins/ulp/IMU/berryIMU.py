4dcef8cfbe3023c7efc4f59d5563b9d1fc633d7a
Byron Watts <btwatts@gmail.com>
2024-04-01 03:14:01 +0800

    Change output for now.  Remember that output to the screen is temporary.

--- fixgw/plugins/ulp/IMU/berryIMU.py
+++ fixgw/plugins/ulp/IMU/berryIMU.py
@@ -388,23 +388,36 @@ if __name__ == '__main__':
 
           ##################### END Tilt Compensation ########################
 
-          if 1:                       #Change to '0' to stop showing the angles from the accelerometer
-              outputString += "#  ACCX Angle %5.2f ACCY Angle %5.2f  #  " % (AccXangle, AccYangle)
-
-          if 1:                       #Change to '0' to stop  showing the angles from the gyro
-              outputString +="\t# GRYX Angle %5.2f  GYRY Angle %5.2f  GYRZ Angle %5.2f # " % (gyroXangle,gyroYangle,gyroZangle)
-
-          if 1:                       #Change to '0' to stop  showing the angles from the complementary filter
-              outputString +="\t#  CFangleX Angle %5.2f   CFangleY Angle %5.2f  #" % (CFangleX,CFangleY)
-
-          if 1:                       #Change to '0' to stop  showing the heading
-              outputString +="\t# HEADING %5.2f  tiltCompensatedHeading %5.2f #" % (heading,tiltCompensatedHeading)
-
-          if 1:                       #Change to '0' to stop  showing the angles from the Kalman filter
-              outputString +="# kalmanX %5.2f   kalmanY %5.2f #" % (kalmanX,kalmanY)
-
+          print("")
+          print("")
+          print("")
           print(outputString)
 
+          if 1:                       #Change to '0' to stop showing the angles from the accelerometer
+              outputString = ""
+              outputString += "#  ACCX Angle %5.2f ACCY Angle %5.2f  #  " % (AccXangle, AccYangle)
+              print(outputString)
+
+          if 1:                       #Change to '0' to stop  showing the angles from the gyro
+              outputString = ""
+              outputString +="\t# GRYX Angle %5.2f  GYRY Angle %5.2f  GYRZ Angle %5.2f # " % (gyroXangle,gyroYangle,gyroZangle)
+              print(outpuString)
+
+          if 1:                       #Change to '0' to stop  showing the angles from the complementary filter
+              outputString = ""
+              outputString +="\t#  CFangleX Angle %5.2f   CFangleY Angle %5.2f  #" % (CFangleX,CFangleY)
+              print(outputString)
+
+          if 1:                       #Change to '0' to stop  showing the heading
+              outputString = ""
+              outputString +="\t# HEADING %5.2f  tiltCompensatedHeading %5.2f #" % (heading,tiltCompensatedHeading)
+              print(outputString)
+
+          if 1:                       #Change to '0' to stop  showing the angles from the Kalman filter
+              outputString = ""
+              outputString +="# kalmanX %5.2f   kalmanY %5.2f #" % (kalmanX,kalmanY)
+              print(outputString)
+
           #slow program down a bit, makes the output more readable
-          time.sleep(0.3)
+          time.sleep(2)
 
