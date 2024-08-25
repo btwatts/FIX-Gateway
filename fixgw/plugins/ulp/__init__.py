ed91e1f01f3b39417cc2268cab1eb32cf3c22afd
Byron Watts <btwatts@gmail.com>
2024-08-25 14:33:20 +0800

    remove old code

--- fixgw/plugins/ulp/__init__.py
+++ fixgw/plugins/ulp/__init__.py
@@ -70,7 +70,7 @@ class MainThread(threading.Thread):
 
        #self.bmp.initialize()
         if self.bmp.initialize() == None:
-            from fixgw.plugins.ulp.bmp280 import BMP280, BMP280_ADDRESS
+            from fixgw.plugins.ulp.bmp280 import BMP280
             self.bmp = BMP280()
             self.bmp.initialize()
 
