b5b4eaa37cea8a2a2354bb2a234deb90c793870c
Byron Watts <btwatts@gmail.com>
2024-08-25 20:35:09 +0800

    toying with settings...and oops maybe

--- fixgw/plugins/ulp/bmp280.py
+++ fixgw/plugins/ulp/bmp280.py
@@ -526,20 +526,21 @@ if __name__ == '__main__':
  bmp280._BMP280__LoadDefaultCalibrationCoef()
  
  #bmp280.initialize()
- qnh=1013.25
+ seaLevelhPa=1013.25
+
  for x in range(0,2): #while True:
   time.sleep(0.5)
   Values280 = bmp280.Get()
   print(f"object: {Values280}")
   print()
   temperature = Values280.temperature
-  print(f"temp: {temperature}")
+  print(f"temp C: {temperature}")
+  print(f"temp F: {9/5*temperature + 32}")
   pressure = Values280.pressure
   print(f"press: {pressure}")
   localPressure = pressure / 100
-  altitude = ((pow((qnh / localPressure), (1.0 / 5.257)) - 1) * (temperature + 273.15)) / 0.0065
-  print(f"old alt: {altitude}")
-  newAlt = 44330*(1.0-pow(localPressure / 1013.25, 0.1902949571836346)) #* 100 # Note:  0.1902949571836346 is: 1/5.255
+
+  newAlt = 44330*(1.0 - pow(localPressure / seaLevelhPa, 0.1902949571836346)) #* 100 # Note:  0.1902949571836346 is: 1/5.255
   print(f"new alt: {newAlt}")
   print(f"check alt: {newAlt / 100}")
   #temperature,pressure,altitude = bmp280.get_temperature_and_pressure_and_altitude()
