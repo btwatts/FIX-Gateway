9e07027da2c096d247cd5a6877e9fb613cc47fc0
Byron Watts <btwatts@gmail.com>
2024-10-05 19:30:05 +0800

    Corrected imu reference.

--- fixgw/plugins/ulp/IMU/berryIMU.py
+++ fixgw/plugins/ulp/IMU/berryIMU.py
@@ -323,8 +323,8 @@ class BERRYIMU(object):
         self.CFangleY=AA*(self.CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
 
     #Kalman filter used to combine the accelerometer and gyro values.
-        kalmanY = self.kalmanFilterY(self, AccYangle, rate_gyr_y, LP)
-        kalmanX = self.kalmanFilterX(self, AccXangle, rate_gyr_x, LP)
+        kalmanY = self.kalmanFilterY(AccYangle, rate_gyr_y, LP)
+        kalmanX = self.kalmanFilterX(AccXangle, rate_gyr_x, LP)
 
     #Calculate heading
         heading = 180 * math.atan2(MAGy,MAGx)/M_PI
